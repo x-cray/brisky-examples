2b2fb3eb1ebdb5fec68d8d618f6cdf7d03ca1f71
Denis Parchenko <denis.parchenko@gmail.com>
2016-11-12 19:20:49 +0200

    Implement workspace mode switcher

--- dashboard/index.js
+++ dashboard/index.js
@@ -4,11 +4,15 @@ require('./style.css')
 const render = require('brisky/render')
 const s = require('vigour-state/s')
 
-const contentTypes = ['sports', 'movies', 'kids']
+const WORKSPACE_MODE_DESIGNER = 'designer'
+const WORKSPACE_MODE_STYLER = 'styler'
+
+const contentTypes = ['sports', 'movies', 'kids', 'music']
 const dataSources = {
   sports: [''],
   movies: ['movies feed'],
-  kids: ['cartoons feed']
+  kids: ['cartoons feed'],
+  music: []
 }
 const layouts = {
   sports: ['layout 1', 'layout 2'],
@@ -19,19 +23,45 @@ const components = {
 
 }
 
+function getNewProject (name) {
+  return {
+    name,
+    created: new Date(),
+    workspace: {
+      mode: WORKSPACE_MODE_DESIGNER
+    }
+  }
+}
+
+function getModeSwitchButton (name, mode) {
+  return {
+    tag: 'li',
+    link: {
+      tag: 'a',
+      text: name,
+      props: { href: 'javascript:void(0)' },
+      class: {
+        selected: {
+          $: '$test',
+          $test: state => state.mode && state.mode.compute() === mode,
+          $transform: true
+        }
+      },
+      on: {
+        click: (e, stamp) => e.state.set({ mode: mode }, stamp)
+      }
+    }
+  }
+}
+
 const state = s({
   projects: {
-    mtv: {
-      created: new Date(),
-      name: 'MTV App'
-    },
-    demo: {
-      created: new Date(),
-      name: 'Demo'
-    }
+    mtv: getNewProject('MTV App'),
+    demo: getNewProject('Demo')
   },
   lastCreatedProject: 0
 })
+state.set({ selectedProject: state.projects.mtv })
 
 const projectItem = {
   tag: 'li',
@@ -40,12 +70,12 @@ const projectItem = {
     text: {
       $: 'name'
     },
-    props: { href: '#' },
+    props: { href: 'javascript:void(0)' },
     class: {
       selected: {
         $: '$test',
         $test: {
-          val: state => state.root.selectedProject && state.root.selectedProject.origin() === state,
+          val: state => state.root.selectedProject && state.root.selectedProject.compute() === state,
           $: {
             $root: { selectedProject: true }
           }
@@ -61,15 +91,12 @@ const projectItem = {
 
 const addNewProject = {
   tag: 'a',
-  props: { href: '#' },
+  props: { href: 'javascript:void(0)' },
   text: 'add',
   on: {
     click: (e, stamp) => {
       const newProjectIndex = e.state.root.lastCreatedProject.compute() + 1
-      const newProject = {
-        created: new Date(),
-        name: 'New project ' + newProjectIndex
-      }
+      const newProject = getNewProject('New project ' + newProjectIndex)
       e.state.root.set({
         lastCreatedProject: newProjectIndex,
         projects: { [newProjectIndex]: newProject }
@@ -78,8 +105,48 @@ const addNewProject = {
   }
 }
 
-const workspace = {
+const modeSwitch = {
+  $: 'workspace',
+  tag: 'ul',
+  class: 'tabs',
+  designerMode: getModeSwitchButton('Designer', WORKSPACE_MODE_DESIGNER),
+  stylerMode: getModeSwitchButton('Styler', WORKSPACE_MODE_STYLER)
+}
 
+const workspace = {
+  $: 'workspace',
+  designer: {
+    $: '$test',
+    $test: state => state.mode && state.mode.compute() === WORKSPACE_MODE_DESIGNER,
+    class: 'workspace',
+    header: {
+      tag: 'h3',
+      text: 'Designer'
+    }
+  },
+  styler: {
+    $: '$test',
+    $test: state => state.mode && state.mode.compute() === WORKSPACE_MODE_STYLER,
+    class: 'workspace',
+    header: {
+      tag: 'h3',
+      text: 'This page allows you to change your app style'
+    },
+    palette: {
+      header: {
+        tag: 'p',
+        text: 'Pick your color palette below'
+      },
+      colors: {
+        class: 'palette',
+        swatch1: { style: { backgroundColor: '#f15d58' } },
+        swatch2: { style: { backgroundColor: '#363635' } },
+        swatch3: { style: { backgroundColor: '#83bf17' } },
+        swatch4: { style: { backgroundColor: '#a68f58' } },
+        swatch5: { style: { backgroundColor: '#dcddcd' } }
+      }
+    }
+  }
 }
 
 const dashboardApp = {
@@ -99,18 +166,18 @@ const dashboardApp = {
     }
   },
   workspaceSection: {
-    $: '$test',
-    $test: {
-      val: state => !!state.root.selectedProject
-    },
+    $: '$root.selectedProject',
     tag: 'section',
     class: 'workspace-pane',
     header: {
-      tag: 'h2',
       class: 'pane-header',
-      text: {
-        $: '$root.selectedProject.name'
-      }
+      title: {
+        tag: 'h2',
+        text: {
+          $: 'name'
+        }
+      },
+      modeSwitch
     },
     workspace
   }
